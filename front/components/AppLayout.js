bd343b795e8fea8081953dbd5bb5e529676bb009
ImhyeMi <jazzyfact2000@gmail.com>
2021-07-26 22:59:42 +0900

    [ADD] Link, eslint 설정

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import PropTpyes from 'prop-types';
+import Link from 'next/link';
 
 const AppLayout = ({children}) => {
     return (
     <div>
-        <div>공통메뉴</div>
+      <Link href="/"><a>노드버드</a></Link>
+      <Link href="/profile"><a>프로필</a></Link>
+      <Link href="/signup"><a>회원가입</a></Link>
         {children}
     </div>)
 }
 AppLayout.prototype = {
+    //children은 node 
     children: PropTpyes.node.isRequired,
 
 };
