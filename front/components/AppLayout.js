e4512593e5754e257b398cee5bd66aab69bcb593
ImhyeMi <jazzyfact2000@gmail.com>
2021-08-12 22:30:40 +0900

    [ADD] ant styled 설치

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import PropTpyes from 'prop-types';
 import Link from 'next/link';
+import {Menu} from 'antd';
 
 const AppLayout = ({children}) => {
     return (
     <div>
+        <Menu mode ="horizontal">
+            <Menu.Item>
       <Link href="/"><a>노드버드</a></Link>
+            </Menu.Item>
+            <Menu.Item>
       <Link href="/profile"><a>프로필</a></Link>
+            </Menu.Item>
+            <Menu.Item>
       <Link href="/signup"><a>회원가입</a></Link>
+            </Menu.Item>
+      </Menu>
         {children}
+
     </div>)
 }
 AppLayout.prototype = {
