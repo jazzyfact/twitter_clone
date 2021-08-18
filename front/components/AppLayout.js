885e9e28b55087fdc6b4314263c4f169579a5b5a
ImhyeMi <jazzyfact2000@gmail.com>
2021-08-18 23:52:45 +0900

    [ADD] MENU 반응형 작업 중

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,31 +1,43 @@
 import React from 'react';
-import PropTpyes from 'prop-types';
 import Link from 'next/link';
-import {Menu} from 'antd';
-
+import PropTypes from 'prop-types';
+import { Col, Input, Menu, Row } from 'antd';
 
 const AppLayout = ({children}) => {
     return (
     <div>
         <Menu mode ="horizontal">
             <Menu.Item>
-      <Link href="/"><a>노드버드</a></Link>
+                <Link href="/"><a>노드버드</a></Link>
             </Menu.Item>
             <Menu.Item>
-      <Link href="/profile"><a>프로필</a></Link>
+                <Link href="/profile"><a>프로필</a></Link>
+            </Menu.Item>
+            <Menu.Item>
+                <Input.Search enterButton style={{verticalAlign: 'middle'}}/>
             </Menu.Item>
             <Menu.Item>
       <Link href="/signup"><a>회원가입</a></Link>
             </Menu.Item>
       </Menu>
+      <Row>
+        <Col  xs= {24} md={6}>
+            왼쪽 메뉴
+        </Col> 
+        <Col  xs= {24} md={12}>
         {children}
-
-    </div>)
-}
-AppLayout.prototype = {
-    //children은 node 
-    children: PropTpyes.node.isRequired,
-
+        </Col>
+        <Col  xs= {24} md={6}>
+            오른쪽 메뉴
+        </Col>
+        
+      </Row>
+    </div>
+    );
 };
 
+AppLayout.propTypes = {
+    children: PropTypes.node.isRequired,
+}
+  
 export default AppLayout;
