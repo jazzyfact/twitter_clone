b8dc16bb1d736c824811c8ffaf30a828dbcfe696
ImhyeMi <jazzyfact2000@gmail.com>
2021-08-26 00:23:28 +0900

    [ADD] 회원가입 커스텀 훅 사용

--- front/components/LoginForm.js
+++ front/components/LoginForm.js
@@ -2,39 +2,22 @@ import React,{useState, useCallback, useMemo} from 'react';
 import {Form, Input, Button} from 'antd';
 import Link  from 'next/link';
 import styled from 'styled-components';
-import PropTypes from 'prop-tyeps';
+import PropTypes from 'prop-types';
+import useInput from '../hooks/useInput';
 
 
 
 // styled이 적용된, styled 태그를 불러옴
 const ButtonWrapper = styled.div`
-    margin-Top: 10px` ;
+    margin-Top: 10px` ; 
+
+const FormWrapper = styled.div`
+    margin-Top: 10px` ; 
 
 const LoginForm = ({setIsLoggedIn}) => {
-
-    const [id, setId] = useState('');
-    const [password, setPassword] = useState('');
-   
-
-    const onChangeId = useCallback((e) => {
-
-        setId(e.target.value);
-    }, []);
-
-    const onChangePassword = useCallback((e) => {
-
-        setPassword(e.target.value);
-    }, []);
-
-
-    const style = useMemo(() => ({
-        marginTop:  10
-    }), []);
-
-    const FormWrapper = styled(Form)`
-        padding:10px
-    `;
-
+    
+    const [id, onChangeId] = useInput('');
+    const [password, onChangePassword] = useInput('');
 
     const onSubmitForm = useCallback(()  => {
         console.log(id, password);
@@ -42,6 +25,12 @@ const LoginForm = ({setIsLoggedIn}) => {
     }, [id, password]); 
 
     
+
+ 
+
+
+
+  
     return (
         <FormWrapper onFinish={onSubmitForm}>
             <div>
@@ -63,7 +52,7 @@ const LoginForm = ({setIsLoggedIn}) => {
                 onChange={onChangePassword} 
                 required />
             </div>
-            <ButtonWrapper style ={style}>
+            <ButtonWrapper >
                 {/* style은 객체로 넣으면 안된다 */}
                 <Button type="primary" htmlType="submit" loading={false}>로그인 </Button>
                 <Link href="/signup"><a><Button>회원가입</Button></a></Link>
--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,32 +1,54 @@
-import React ,{useCallback, usestate} from 'react';
+import React ,{useCallback, useState} from 'react';
 import Head from 'next/head';
-import {Form, Input } from 'antd';
+import {Form, Input,Checkbox,Button } from 'antd';
+import styled  from 'styled-components'
+
 import AppLayout from '../components/AppLayout';
+import useInput from '../hooks/useInput';
+
+const ErrorMessage = styled.div `
+    color : red;
+    `;
 
 const Signup = () => {
 
-    const [id, setId] = useState('');
-    const onChangeId = useCallback((e) => {
-            setId(e.target.value)
-        },[]);
-
-    const [nickname, setNick] = useState('');
-    const onChangeNickname = useCallback((e) => {
-            setNick(e.target.value)
-        },[]);
+    //hooks도 중복되면 중복 제거
+    const [id, onChangeId] = useInput('');
+    const [nickname, onChangeNickname] = useInput('')
+    const [password, onChangePassword] = useInput('')
     
+    const [passwordCheck, setPasswordCheck] = useState('');
+    //비밀번호 = 비밀번호 확인 false, != true
+    const [passwordError, setPasswordError] = useState(false);
+    
+    //중복체크해줘야 해서 차이가 남
+    const onChangePasswordCheck = useCallback((e) => {
+            setPasswordCheck(e.target.value);
+            setPasswordError(e.target.value !== password);//패스워드 체크 여부 추가, 커스텀 훅으로 사용 할 수 없음
+        },[password],
+    )
 
-    const [password, setPassword] = useState('');
-    const onChangePassword = useCallback((e) => {
-        setPassword(e.target.value)
-    },[]);
+    const [term, setTerm] = useState('');
+    const [termError, setTermError] = useState(false);
+    const onChangeTerm = useCallback((e) => {
+        setTerm(e.target.checked);
+        setTermError(false);
+    }, []);
 
-    const onSubmit = () => {
-        const onSubmit = useCallback(() => {
+    
+    //한번더 체크
+    const onSubmit = useCallback(() => {
+        if(password !== passwordCheck) {
+            return setPasswordError(true);
+        }
 
-        }, []);
+        if(!term) {
+            return setTermError(true);
+        }
+        console.log(id,nickname,password);
+    }, [password, passwordCheck,term]);
 
-    }
+    
     return (
         <AppLayout>
         <Head>
@@ -57,7 +79,17 @@ const Signup = () => {
                     type="pasword" 
                     value={passwordCheck} 
                     required 
-                    onChange={onChangePasswordCheck} />
+                    onChange={onChangePasswordCheck} 
+                    />
+                    {/* 비밀번호 에러가 있으면 red 표시 */}
+                    {passwordError && <ErrorMessage> 비밀전호가 일치하지 않습니다. </ErrorMessage>}
+            </div>
+            <div>
+                <Checkbox name="user-term" checked={term} onChange={onChangeTerm} >이용 약관 동의합니다</Checkbox>
+                {termError && <ErrorMessage>약관에 동의하셔야합니다. </ErrorMessage>}
+            </div>
+            <div style={{marginTop :10}}>
+                <Button type ="primary" htmlType="submit"> 가입하기</Button>
             </div>
         </Form>
        </AppLayout>
