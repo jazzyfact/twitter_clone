3c95a36b5d72c7abaa35101853ab07ab2a6a4647
ImhyeMi <jazzyfact2000@gmail.com>
2021-09-29 02:55:36 +0900

    [ADD] 노드로 서버 구동하기

--- front/components/LoginForm.js
+++ front/components/LoginForm.js
@@ -1,64 +1,60 @@
-import React,{useCallback} from 'react';
-import {Form, Input, Button} from 'antd';
-import Link  from 'next/link';
+import React, { useCallback } from 'react';
+import { Form, Input, Button } from 'antd';
+import Link from 'next/link';
 import styled from 'styled-components';
-import {useDispatch, useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import useInput from '../hooks/useInput';
-import {loginRequestAction} from '../reducers/user';
-
-// const ButtonWrapper = styled.div`
-//   margin-top :10px;
-// `;
-// const FormWrapper = styled(Form)`
-//   padding : 10px
-// `;
-
+import { LOG_IN_REQUEST } from '../reducers/user';
 
 // styled이 적용된, styled 태그를 불러옴
 const ButtonWrapper = styled.div`
-    margin-Top: 10px` ; 
+  margin-top: 10px;
+`;
 
-const FormWrapper = styled.div`
-    margin-Top: 10px` ; 
+const FormWrapper = styled(Form)`
+  padding: 10px;
+`;
 
 const LoginForm = () => {
-    const dispatch = useDispatch();
-    const { logInLoading } = useSelector((state) => state.user);
-    const [email, onChangeEmail] = useInput('');
-    const [password, onChangePassword] = useInput('');
-  
+  const dispatch = useDispatch();
+  const { logInLoading } = useSelector((state) => state.user);
+  const [email, onChangeEmail] = useInput('');
+  const [password, onChangePassword] = useInput('');
 
-    const onSubmitForm = useCallback(() => {
-        console.log(email, password);
-        dispatch(loginRequestAction({ email, password,
-        }));
-      }, [email, password]);
-    
+  const onSubmitForm = useCallback(() => {
+    console.log(email, password);
+    dispatch({
+      type: LOG_IN_REQUEST,
+      data: { email, password },
+    });
+  }, [email, password]);
 
- 
-      return (
-        <Form onFinish={onSubmitForm} style={{ padding: '10px' }}>
-          <div>
-            <label htmlFor="user-email">이메일</label>
-            <br />
-             {/* 컴포넌트에 props로 넘겨주는 것은 useCallback 써야함 */}
-            <Input name="user-email"  type="email" value={email} onChange={onChangeEmail} required />
-          </div>
-          <div>
-            <label htmlFor="user-password">비밀번호</label>
-            <br />
-            <Input name="user-password" value={password} onChange={onChangePassword} type="password" required />
-          </div>
-          <div style={{ marginTop: '10px' }}>
-            <Button type="primary" htmlType="submit" loading={logInLoading}>로그인</Button>
-            <Link href="/signup"><a><Button>회원가입</Button></a></Link>
-          </div>
-        </Form>
-      );
-    };
-
-
-
+  return (
+    <FormWrapper onFinish={onSubmitForm}>
+      <div>
+        <label htmlFor="user-email">이메일</label>
+        <br />
+            {/* 컴포넌트에 props로 넘겨주는 것은 useCallback 써야함 */}
+        <Input name="user-email" type="email" value={email} onChange={onChangeEmail} required />
+      </div>
+      <div>
+        <label htmlFor="user-password">비밀번호</label>
+        <br />
+        <Input
+          name="user-password"
+          type="password"
+          value={password}
+          onChange={onChangePassword}
+          required
+        />
+      </div>
+      <ButtonWrapper>
+        <Button type="primary" htmlType="submit" loading={logInLoading}>로그인</Button>
+        <Link href="/signup"><a><Button>회원가입</Button></a></Link>
+      </ButtonWrapper>
+    </FormWrapper>
+  );
+};
 
 export default LoginForm;
--- front/components/PostForm.js
+++ front/components/PostForm.js
@@ -1,61 +1,58 @@
-import React,{ useCallback,  useRef, useEffect, useState} from 'react'
-import { Form, Input, Button } from 'antd'
+import React, { useCallback, useState, useEffect, useRef } from 'react';
+import { Form, Input, Button } from 'antd';
 import { useSelector, useDispatch } from 'react-redux';
 
+import { ADD_POST_REQUEST } from '../reducers/post';
 
-import { addPost } from '../reducers/post';
-import useInput from '../hooks/useInput';
+const PostForm = () => {
+  const dispatch = useDispatch();
+  const [text, setText] = useState('');
+  const { imagePaths, addPostLoading, addPostDone } = useSelector((state) => state.post);
 
+  const imageInput = useRef();
+  const onClickImageUpload = useCallback(() => {
+    imageInput.current.click();
+  }, [imageInput.current]);
 
-const PostForm = () =>{
-    const dispatch = useDispatch();
-    const [text, onChangeText , setText] = useInput('');
-    const { imagePaths, addPostLoading, addPostDone } = useSelector((state) => state.post);
- 
-  
-  
-    //메세지 보내고 초기화
-    useEffect(() => {
-        if (addPostDone) {
-          setText('');
-        }
-      }, [addPostDone]);
+//메세지 보내고 초기화
+  useEffect(() => {
+    if (addPostDone) {
+      setText('');
+    }
+  }, [addPostDone]);
 
- 
-    const onSubmit = useCallback(() => {
-        dispatch(addPost(text));
-    }, [text]);
+  const onSubmitForm = useCallback(() => {
+    dispatch({
+      type: ADD_POST_REQUEST,
+      data: text,
+    });
+  }, [text]);
 
-    const imageInput = useRef();
-    const onClickImageUpload = useCallback(() =>{
-        imageInput.current.click();
-    },[imageInput.current]);
-    return(
-        <Form style={{ margin: '10px 0 20px'}} encType="multipart/form-data" onFinish={onSubmit}>
-            <Input.TextArea
-                value={text}
-                onChange={onChangeText}
-                maxLength={140}
-                placeholder="어떤 신기한 일이 있었나요"
-            />
-           <div>
-               {/* ref 실제 DOM에 접근하기 위해 */}
-               <input type="file" multiple hidden  ref={imageInput}/>
-               <Button onClick={onClickImageUpload}>이미지 업로드</Button>
-               <Button type="primary" style={{float : 'right'}} htmlType ="submit">짹짹</Button>
-           </div>
-           <div>
-               {imagePaths.map((v) => (
-                    <div key={v} style={{ display : 'inline-block'}}>
-                       <img src={v} style={{ width : '200px' }} alt = {v} />
-                        <div>
-                            <Button>제거</Button>
-                        </div>
-                    </div>
-               ))}
-           </div>
-        </Form>
-    )
-}
+  const onChangeText = useCallback((e) => {
+    setText(e.target.value);
+  }, []);
+
+  return (
+    <Form style={{ margin: '10px 0 20px' }} encType="multipart/form-data" onFinish={onSubmitForm}>
+      <Input.TextArea maxLength={140} placeholder="어떤 신기한 일이 있었나요?" value={text} onChange={onChangeText} />
+      <div>
+         {/* ref 실제 DOM에 접근하기 위해 */}
+        <input type="file" multiple hidden ref={imageInput} />
+        <Button onClick={onClickImageUpload}>이미지 업로드</Button>
+        <Button type="primary" style={{ float: 'right' }} htmlType="submit" loading={addPostLoading}>짹짹</Button>
+      </div>
+      <div>
+        {imagePaths.map((v) => (
+          <div key={v} style={{ display: 'inline-block' }}>
+            <img src={`http://localhost:3065/${v}`} style={{ width: '200px' }} alt={v} />
+            <div>
+              <Button>제거</Button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </Form>
+  );
+};
 
 export default PostForm;
