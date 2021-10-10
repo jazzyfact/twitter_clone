c1895fdaf69a135914165704ff7bf86eddf7c61f
ImhyeMi <jazzyfact2000@gmail.com>
2021-10-10 23:03:21 +0900

    [ADD] 게시글, 댓글 작성 추가

--- back/routes/post.js
+++ back/routes/post.js
@@ -1,9 +1,45 @@
 const express =require('express');
 
+const { Post } =require('../models');
+const { isLoggedIn } = require('./middlewares');
+
 const router = express.Router();
 
-router.post('/', (req, res) => {  //POST, /post
-    res.json({id : 1, content : 'hello'});
+router.post('/',isLoggedIn, async (req, res) => {  //POST, /post
+    try{
+      const post = await Post.create({
+        content : req.body.content,
+        UserId : req.user.id,
+        });
+         res.status(201).json(post);
+    }catch(error){
+        console.error(error);
+        next(error);
+    }
+});
+
+//댓글
+router.post('/:postId/comment', isLoggedIn, async (req, res) => {  //POST, /post/1/comment
+    try{
+        await Post.findOne({
+            where : { id : req.params.postId},
+        });
+        if(!post){
+            return res.status(403).send('존재하지 않는 게시글입니다');
+        }
+        const comment = await Comment.create({
+            content : req.body.content,
+            PostId : req.params.postId,
+            UserId : req.user.id,
+        })
+      const post = await Post.create({
+        content : req.body.content,
+        });
+         res.status(201).json(post);
+    }catch(error){
+        console.error(error);
+        next(error);
+    }
 });
 
 router.delete('/', (req, res) => { //DELETE /post
